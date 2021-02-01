db05cd2655bf2dc59badf81bf5542de140259b10
bdacoscos <briana.dacoscos@gmaill.com>
2021-02-01 14:52:32 -0800

    custom header exercise

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
   res.send('Hey, girl... you did it!')
 });
 
+app.get('/tea', (req, res) => {
+  res.set('X-awesome-header', 'catcatcat');
+  res.status(418);
+  res.send('tea is awesome');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
